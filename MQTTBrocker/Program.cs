1245aa497719f8e7ec036b6e56d2903640624fff
root <deniska1234denis@gamil.com>
2026-06-14 17:23:03 +0700

    initialize

---
+++ MQTTBrocker/Program.cs
@@ -0,0 +1,36 @@
+﻿using MQTTnet.Protocol;
+using MQTTnet.Server;
+using System.Net;
+
+var mqttServerFactory = new MqttServerFactory();
+var mqttServerOptions = new MqttServerOptionsBuilder()
+    .WithDefaultEndpoint()
+    .WithDefaultEndpointPort(1883)
+    .WithDefaultEndpointBoundIPAddress(IPAddress.Any)
+    .Build();
+
+
+using var mqttServer = mqttServerFactory.CreateMqttServer(mqttServerOptions);
+
+mqttServer.ValidatingConnectionAsync += e =>
+{
+    e.ReasonCode = MqttConnectReasonCode.Success;
+    return Task.CompletedTask;
+};
+
+mqttServer.InterceptingPublishAsync += e =>
+{
+    Console.WriteLine(e.ApplicationMessage);
+    Console.WriteLine(e.ApplicationMessage.Payload);
+    return Task.CompletedTask;
+};
+mqttServer.ClientConnectedAsync += e =>
+{
+    Console.WriteLine(e.ClientId);
+    return Task.CompletedTask;
+};
+
+await mqttServer.StartAsync();
+Console.WriteLine("MQTT Broker started. Press Enter to exit.");
+Console.ReadLine();
+await mqttServer.StopAsync();
