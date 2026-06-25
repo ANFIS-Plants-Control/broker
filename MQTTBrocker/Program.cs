1521ba3c3ffb87e99d3461e20ed36cd4d2dbe304
root <deniska1234denis@gamil.com>
2026-06-25 09:46:46 +0700

    modify program

--- MQTTBrocker/Program.cs
+++ MQTTBrocker/Program.cs
@@ -1,48 +1,39 @@
-﻿using MQTTnet.Protocol;
+﻿
+using MQTTnet.Protocol;
 using MQTTnet.Server;
 using System.Net;
 using System.Text;
 
-namespace MQTTBroker
+var mqttServerFactory = new MqttServerFactory();
+var mqttServerOptions = new MqttServerOptionsBuilder()
+    .WithDefaultEndpoint()
+    .WithDefaultEndpointPort(1883)
+    .WithDefaultEndpointBoundIPAddress(IPAddress.Any)
+    .Build();
+
+
+using var mqttServer = mqttServerFactory.CreateMqttServer(mqttServerOptions);
+
+mqttServer.ValidatingConnectionAsync += e =>
 {
-    internal class MQTTBrocker
-    {
-        private async static Task Main(string[] args)
-        {
-            var mqttServerFactory = new MqttServerFactory();
-            var mqttServerOptions = new MqttServerOptionsBuilder()
-                .WithDefaultEndpoint()
-                .WithDefaultEndpointPort(1883)
-                .WithDefaultEndpointBoundIPAddress(IPAddress.Any)
-                .Build();
+    e.ReasonCode = MqttConnectReasonCode.Success;
+    return Task.CompletedTask;
+};
 
+mqttServer.InterceptingPublishAsync += e =>
+{
+    Console.WriteLine(e.ApplicationMessage);
+    Console.WriteLine(e.ApplicationMessage.Payload);
+    var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+    Console.WriteLine(payload);
+    return Task.CompletedTask;
+};
+mqttServer.ClientConnectedAsync += e =>
+{
+    Console.WriteLine(e.ClientId);
+    return Task.CompletedTask;
+};
 
-            using var mqttServer = mqttServerFactory.CreateMqttServer(mqttServerOptions);
-
-            mqttServer.ValidatingConnectionAsync += e =>
-            {
-                e.ReasonCode = MqttConnectReasonCode.Success;
-                return Task.CompletedTask;
-            };
-
-            mqttServer.InterceptingPublishAsync += e =>
-            {
-                Console.WriteLine(e.ApplicationMessage);
-                Console.WriteLine(e.ApplicationMessage.Payload);
-                var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
-                Console.WriteLine(payload);
-                return Task.CompletedTask;
-            };
-            mqttServer.ClientConnectedAsync += e =>
-            {
-                Console.WriteLine(e.ClientId);
-                return Task.CompletedTask;
-            };
-
-            await mqttServer.StartAsync();
-            Console.WriteLine("MQTT Broker started. Press Enter to exit.");
-            Console.ReadLine();
-            await mqttServer.StopAsync();
-        }
-    }
-}
+await mqttServer.StartAsync();
+Console.WriteLine("MQTT Broker started. Press Enter to exit.");
+await Task.Delay(Timeout.Infinite);
